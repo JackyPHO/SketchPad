35e9eca271cdd88f4d506d140bdf557136d8d01b
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-10-23 08:45:32 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import "./style.css";
 
-const APP_NAME = "PooPoo";
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const APP_NAME = "SketchPad";
+const title = document.querySelector<HTMLDivElement>("h1")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+title.innerHTML = APP_NAME;
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+if (ctx) {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 256, 256);
+}
