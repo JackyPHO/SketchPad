f34f7c25bdbb0df70428f42e9154a6f3ca02fded
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-10-23 10:13:07 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,31 +9,44 @@ title.innerHTML = APP_NAME;
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-if (ctx) {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, 256, 256);
+function blank(ctx:CanvasRenderingContext2D|null){
+    if(ctx){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 256, 256);
+    }
 }
+blank(ctx);
+
+interface Point{
+    x: number,
+    y: number
+}
+let p: Point[] = [];
+const event = new Event("drawing-changed");
+//Observer function for "drawing-changed"
+function draw(){
+    const p1 = p[p.length - 1];
+    const p2 = p[p.length - 2];
+    drawLine(ctx, p1.x, p1.y, p2.x, p2.y);
+}
+canvas.addEventListener('drawing-changed', draw)
+
 let isDrawing = false;
-let x = 0;
-let y = 0;
+
 canvas.addEventListener("mousedown", (e) => {
-    x = e.offsetX;
-    y = e.offsetY;
     isDrawing = true;
+    p.push({x:e.offsetX, y:e.offsetY});
 })
 canvas.addEventListener("mousemove", (e) => {
     if (isDrawing) {
-      drawLine(ctx, x, y, e.offsetX, e.offsetY);
-      x = e.offsetX;
-      y = e.offsetY;
+      p.push({x:e.offsetX, y:e.offsetY});
+      canvas.dispatchEvent(event);
     }
   });
+
   
 globalThis.addEventListener("mouseup", (e) => {
     if (isDrawing) {
-      drawLine(ctx, x, y, e.offsetX, e.offsetY);
-      x = 0;
-      y = 0;
       isDrawing = false;
     }
 });
@@ -54,8 +67,6 @@ b1.className = "button";
 b1.textContent = "Clear";
 app.append(b1);
 b1.addEventListener("click", function () {
-    if (ctx){
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, 256, 256);
-    }
+    blank(ctx);
+    p = [];
 });
